a1c7b21e7b7ac9e3ab0d58bbbdf7d0ef88a121de
TRINADH KOYA <trinadhkoya9@gmail.com>
2018-01-31 19:55:47 +0530

    HackerRank Problem WIP

---
+++ practices/challenges/diagonal-difference.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    n = int(input().strip())
+    a = []
+    for i in range(n):
+        print(i)
+
+
+    print(n)
---
+++ practices/challenges/plus-minus.py
@@ -0,0 +1,29 @@
+import re
+
+
+def plusMinus(arr):
+    pos = 0
+    neg = 0
+    zero = 0
+
+    for i in arr:
+        if i > 0:
+            pos += 1
+        elif i < 0:
+            neg += 1
+        else:
+            zero += 1
+
+    print(pos)
+    print(neg)
+    print(zero)
+
+    print(format(pos/len(arr),".6f"))
+    print(format(neg/len(arr),".6f"))
+    print(format(zero/len(arr),".6f"))
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    arr = list(map(int, input().strip().split(' ')))[:n]
+    plusMinus(arr)
